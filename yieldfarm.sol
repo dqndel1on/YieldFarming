c5d7f2d0d018055dbec699fb96046f944933610b
sanjipun <iamsanjipun@gmail.com>
2022-02-13 20:24:08 +0545

    Yield Farm with 35pa reward

--- yieldfarm.sol
+++ yieldfarm.sol
@@ -13,29 +13,27 @@ contract YieldFarm {
     uint public rate = 35;
     // AggregatorV3Interface internal priceFeed;
 
-    GLT public glt;
+    address public glt;
     
     constructor() {
-        glt = new GLT();
+        glt = 0xd9145CCE52D386f254917e481eB44e9943F39138;
         // priceFeed = AggregatorV3Interface(0x9326BFA02ADD2366b30bacB125260Af641031331);
         priceFeed = 10000; //for vm, 1 eth = 10000 glt
     }
 
-    function getgltBalance() view public returns(uint) {
-        return IERC20(glt).balanceOf(msg.sender);
-    }
     mapping(address => uint) public stakedAmount;
     mapping(address => bool) public isStaking;
     mapping(address => uint) public stakedTime;
 
-    function stakeToken() public payable {
-        stakedAmount[msg.sender] += msg.value;
+    function stakeToken(uint _amount) public payable {
+        IERC20(glt).transferFrom(msg.sender,address(this),_amount);
+        stakedAmount[msg.sender] = _amount;
         isStaking[msg.sender] = true;
         stakedTime[msg.sender] = block.timestamp;
     }
 
     function getStakedToken() view public returns(uint) {
-        return address(this).balance;
+        return IERC20(glt).balanceOf(address(this));
     }
 
     function getStakedTokenByAddress() view public returns(uint) {
@@ -58,17 +56,20 @@ contract YieldFarm {
     } 
 
     function getAccumulatedReward() view public  returns(uint) {
-        return getStakedPriceWithTime().mul((rate.add(36500))).div(52560000);
-    }
-
-    function getStakedPriceWithTime() view public returns(uint) {
-        return (getStakeTime()).mul(priceFeed).mul(getStakedTokenByAddress());
+        uint principal = stakedAmount[msg.sender];
+        for(uint i=0;i<getStakeTime();i++){
+            principal += principal.mul(rate).div(52560000);
+        }
+        return principal;
     }
 
     function getStakeTime() view public returns(uint){
-        return block.timestamp.sub(stakedTime[msg.sender]).div(86400);
+        return block.timestamp.sub(stakedTime[msg.sender]).div(60);
     }
 
+    function getBalanceOfGlt(address _add) view public returns(uint){
+        return IERC20(glt).balanceOf(_add);
+    }
 }
 
 // Features:
